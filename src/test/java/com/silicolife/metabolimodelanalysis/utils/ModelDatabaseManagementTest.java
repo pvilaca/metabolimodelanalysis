8ecde7eddd8f0bf7642c5f0e84d5e4f7540402c3
pvilaca <pvilaca@macbook>
2015-05-28 18:26:13 +0100

    remove all code

--- src/test/java/com/silicolife/metabolimodelanalysis/utils/ModelDatabaseManagementTest.java
+++ src/test/java/com/silicolife/metabolimodelanalysis/utils/ModelDatabaseManagementTest.java
@@ -2,12 +2,20 @@ package com.silicolife.metabolimodelanalysis.utils;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Test;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.io.readers.ErrorsException;
+import pt.uminho.ceb.biosystems.mew.biocomponents.container.io.readers.JSBMLReader;
+import pt.uminho.ceb.biosystems.mew.biocomponents.validation.io.JSBMLValidationException;
 import pt.uminho.ceb.biosystems.mew.utilities.datastructures.map.MapUtils;
 
 public class ModelDatabaseManagementTest {
@@ -32,19 +40,19 @@ public class ModelDatabaseManagementTest {
 		System.out.println(ModelDatabaseManagement.getInstance().getRepeatedModel());
 	}
 	
-	@Test
+//	@Test
 	public void printRepeateadModel(){
 		System.out.println("RepeatedModel");
 		MapUtils.prettyPrint(ModelDatabaseManagement.getInstance().getFilteredRepeatedModel());
 	}
 	
-	@Test
+//	@Test
 	public void printRepeatedDoi(){
 		System.out.println("RepeatedDoi");
 		MapUtils.prettyPrint(ModelDatabaseManagement.getInstance().getFilteredRepeatedDoi());
 	}
 	
-	@Test
+//	@Test
 	public void printRepeatedDoidAllInfo() throws IOException{
 		System.out.println("RepeatedDoi All Info");
 		Map<String, Set<String>> allinfo = ModelDatabaseManagement.getInstance().getFilteredRepeatedDoi();
@@ -53,6 +61,47 @@ public class ModelDatabaseManagementTest {
 			Set<String> i = allinfo.get(id);
 			ModelDatabaseManagement.getInstance().printSomeInfo(i);
 			System.out.println();
+			
+		}
+	}
+	
+	@Test
+	public void readSbml(){
+	
+		
+		Map<String, String> info = ModelDatabaseManagement.getInstance().getOriginalSBML();
+		
+		MapUtils.prettyPrint(info);
+		for(String id : info.keySet()){
+			
+			String file = info.get(id);
+			System.out.println(id + "\t" + file);
+			try {
+				JSBMLReader reader = new JSBMLReader(file, "", false);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (XMLStreamException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ErrorsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSBMLValidationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+				System.out.println(e.getProblems().size() + "\t" + e.getProblemsByClass());
+			}
 			
 		}
 	}
